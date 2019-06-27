14fcb1a5ae739a74a1d2ffe45c580f442e17c85f
JoEdjamf <kibalojordy.edjamfeile@ynov.com>
2019-06-27 14:48:48 +0200

    Update mod_header.php
    monitoring project
    
    Co-Authored-By: ousmane8 <ousmane8@users.noreply.github.com>
    Co-Authored-By: Thanuga <thanuga@users.noreply.github.com>

--- modules/common/mod_header.php
+++ modules/common/mod_header.php
@@ -2,7 +2,7 @@
 	<a id="top"></a>
 	<div class='container'>
 		<div class='sixteen columns'>
-			<h1><a href="<?php echo $root; ?>">Etat du Servur</a></h1>
+			<h1><a href="<?php echo $root; ?>">Etat du Serveur</a></h1>
 		</div>
 	</div>
 </header>
